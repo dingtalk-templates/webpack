35e321b828f66d54c256ab251d092dc08ecd372b
icepy <xiangwenwe@foxmail.com>
2017-06-08 14:35:50 +0800

    add balance

--- template/src/store/index.js
+++ template/src/store/index.js
@@ -12,6 +12,9 @@ export default function Store(Vue){
       mutations: {
         increment (state) {
           state.count++
+        },
+        balance (state) {
+          
         }
       }
     });
